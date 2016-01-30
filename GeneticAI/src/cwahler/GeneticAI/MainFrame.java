6bf30598d84cd3771c482c1cf42826241328d0bd
Carl <silence.is.pointless@gmail.com>
2016-01-30 16:55:05 -0500

    Signed-off-by: Carl <silence.is.pointless@gmail.com>

--- GeneticAI/src/cwahler/GeneticAI/MainFrame.java
+++ GeneticAI/src/cwahler/GeneticAI/MainFrame.java
@@ -5,4 +5,19 @@ import javax.swing.*;
 @SuppressWarnings("serial")
 public class MainFrame extends JFrame{
 	
+	public static void main(String[] args) {
+		new MainFrame();
+	}
+	
+	public MainFrame() {
+		layoutFrame();
+		setVisible(true);
+	}
+	
+	public void layoutFrame() {
+		setSize(800, 600);
+		setTitle("GeneticAI");
+		
+	}
+	
 }
